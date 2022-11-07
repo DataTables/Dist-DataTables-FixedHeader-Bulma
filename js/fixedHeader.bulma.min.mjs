7948ce4813df37c8e6d3f9d32323f5a0c283c3fd
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-07 16:18:28 +0000

    43cfde4f36a20955a840ca5301328839c2337bff Release 3.3.1: Build error so releasing
    Sync to source repo @43cfde4f36a20955a840ca5301328839c2337bff

--- js/fixedHeader.bulma.min.mjs
+++ js/fixedHeader.bulma.min.mjs
@@ -1,4 +1,4 @@
 /*! Bulma styling wrapper for FixedHeader
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-bm";import DataTable from"datatables.net-fixedheader";export default DataTable;
+import $ from"jquery";import DataTable from"datatables.net-bm";import"datatables.net-fixedheader";export default DataTable;
