31043f4060694ed11d7c00b62c5afa7ade4cf531
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-06-06 13:11:27 +0100

    3f717a0badf83c94e5d465a6ee8000c50042eb7a New: Dark mode support for DataTables and Bootstrap 5 styling
    Sync to source repo @3f717a0badf83c94e5d465a6ee8000c50042eb7a

--- js/fixedHeader.bulma.min.mjs
+++ js/fixedHeader.bulma.min.mjs
@@ -1,4 +1,4 @@
 /*! Bulma styling wrapper for FixedHeader
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-bm";import FixedHeader from"datatables.net-fixedheader";export default DataTable;
+import jQuery from"jquery";import DataTable from"datatables.net-bm";import FixedHeader from"datatables.net-fixedheader";let $=jQuery;export default DataTable;
